e68e422a8614cc934b0f48b4352d08cc3a322ca2
baoyu <baoyu@sensetime.com>
2020-11-26 11:49:38 +0800

    add train and test script for the clusters

---
+++ Oneline-DLTv1/finetune.sh
@@ -0,0 +1,6 @@
+JOB_NAME=train_deep_homo
+
+srun --mpi=pmi2  -p VI_AIC_1080TI -n1 --gres=gpu:2 --ntasks-per-node=1 \
+   --job-name=${JOB_NAME} \
+python -u train.py --gpus 2 --cpus 8 --lr 0.000064 --batch_size 32 --finetune True \
+   2>&1 | tee logs/${JOB_NAME}.txt &
